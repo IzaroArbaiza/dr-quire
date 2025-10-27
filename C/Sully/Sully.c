c6b340591d15dc3140f0c16baaa77173f644945f
KeyKey03 <izaroarbaiza@gmail.com>
2025-10-27 11:02:07 +0100

    C part of the proyect finalized

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -3,38 +3,26 @@
 #include <unistd.h>
 
 int main() {
-    int i = 5;
-    int x = i - 1;
-    char *code = "#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>"
-        "%cint main(){%c%cint i=%d;%cint x=i-1;%cchar *code=%c%s%c;"
-        "%cif(i<0)%creturn 0;%cchar filename[32];%csprintf(filename,%cSully_%%d.c%c,x);%cFILE *f=fopen(filename,%cw%c);"
-        "%cif(!f)return 1;%cfprintf(f, code, 10,10,10,10, 9,x,10,10,34,code,34,10,10,10,10,34,34,10,34,34,10,10,10,10,10,34,34,10,10,10,34,34,10,10,10);%cfclose(f);"
-        "%cchar compile_cmd[128];%csprintf(compile_cmd,%cgcc -Wall -Wextra -Werror %%s -o Sully_%%d%c,filename,x);%csystem(compile_cmd);"
-        "%cchar exec_cmd[64];%csprintf(exec_cmd,%c./Sully_%%d%c,x);%csystem(exec_cmd);"
-        "%creturn 0;%c}";
-    
-    if (i < 0)
-        return 0;
-    char filename[32];
-    sprintf(filename, "Sully_%d.c", x);
-    FILE *f = fopen(filename, "w");
-    if (!f)
-        return 1;
-    fprintf(f, code, 10,10,
-        10,10,9,x,10,10,34,code,34,
-        10,10,10,10,34,34,10,34,34,
-        10,10,10,
-        10,10,34,34,10,
-        10,10,34,34,10,
-        10, 10);
-    fclose(f);
+	int i = 5;
+	int x = i - 1;
+	char *code = "#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c%cint main() {%c%cint i = %d;%c%cint x = i - 1;%c%cchar *code = %c%s%c;%c%c%cif (i < 0)%c%c%creturn 0;%c%cchar filename[32];%c%csprintf(filename, %cSully/Sully_%%d.c%c, x);%c%cFILE *f = fopen(filename, %cw%c);%c%cif (!f)%c%c%creturn 1;%c%cfprintf(f, code, 10,10,10,10,10,9,x,10,9,10,9,34,code,34,10,10,9,10,9,9,10,9,10,9,34,34,10,9,34,34,10,9,10,9,9,10,9,10,9,10,10,9,10,9,34,34,10,9,10,9,10,9,34,34,10,9,10,10,9,10);%c%cfclose(f);%c%c%cchar compile_cmd[128];%c%csprintf(compile_cmd, %cgcc -Wall -Wextra -Werror %%s -o Sully/Sully_%%d%c, filename, x);%c%csystem(compile_cmd);%c%cchar exec_cmd[64];%c%csprintf(exec_cmd, %c./Sully/Sully_%%d%c, x);%c%csystem(exec_cmd);%c%c%creturn 0;%c}";
 
-    char compile_cmd[128];
-    sprintf(compile_cmd, "gcc -Wall -Wextra -Werror %s -o Sully_%d", filename, x);
-    system(compile_cmd);
-    char exec_cmd[64];
-    sprintf(exec_cmd, "./Sully_%d", x);
-    system(exec_cmd);
-    
-    return 0;
+	if (i < 0)
+		return 0;
+	char filename[32];
+	sprintf(filename, "Sully/Sully_%d.c", x);
+	FILE *f = fopen(filename, "w");
+	if (!f)
+		return 1;
+	fprintf(f, code, 10,10,10,10,10,9,x,10,9,10,9,34,code,34,10,10,9,10,9,9,10,9,10,9,34,34,10,9,34,34,10,9,10,9,9,10,9,10,9,10,10,9,10,9,34,34,10,9,10,9,10,9,34,34,10,9,10,10,9,10);
+	fclose(f);
+
+	char compile_cmd[128];
+	sprintf(compile_cmd, "gcc -Wall -Wextra -Werror %s -o Sully/Sully_%d", filename, x);
+	system(compile_cmd);
+	char exec_cmd[64];
+	sprintf(exec_cmd, "./Sully/Sully_%d", x);
+	system(exec_cmd);
+
+	return 0;
 }
