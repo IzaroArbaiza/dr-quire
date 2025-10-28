5d7445b2b2bbd75d7dddf6ce062dd889fe87d540
KeyKey03 <izaroarbaiza@gmail.com>
2025-10-28 19:32:45 +0100

    starting the colleen in assembly

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,16 +1,17 @@
 section .data
-msg:    db "Hello", 10	;"Hello\n"
-len:    equ $ - msg		;num of bytes that msg occupies
+msg: db "section .data", 10, "msg: db ", 34, 34, 10, "len: equ $ - msg", 10, 10, "section .text", 10
+len: equ $ - msg		;num of bytes that msg occupies
 
 section .text
 global _start
-_start:
-    mov rax, 1          ;sys_write
-    mov rdi, 1          ;fd = stdout
-    mov rsi, msg        ;buf
-    mov rdx, len        ;count
-    syscall
 
-    mov rax, 60         ;sys_exit
-    xor rdi, rdi
-    syscall
+_start:
+	mov rax, 1			;sys_write
+	mov rdi, 1			;fd = stdout
+	mov rsi, msg		;buf
+	mov rdx, len		;count
+	syscall
+
+	mov rax, 60			;sys_exit
+	xor rdi, rdi
+	syscall
