e018e22246b2a83c32cb97bfe7b8c299fb4f8d9b
KeyKey03 <izaroarbaiza@gmail.com>
2026-02-04 17:48:12 +0100

    something working

--- ASM/Colleen/Colleen.asm
+++ ASM/Colleen/Colleen.asm
@@ -1,68 +1,18 @@
 section .text
-global _start
+    global main
+    extern printf
 
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, s
-    mov rdx, 333
-    syscall
-
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, q
-    mov rdx, 1
-    syscall
-
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, s
-    mov rdx, 333
-    syscall
-
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, q
-    mov rdx, 1
-    syscall
-
-    mov rax, 60
-    xor rdi, rdi
-    syscall
+main:
+    push rbp
+    mov rbp, rsp
+    lea rdi, [rel s]
+    mov rsi, 10
+    mov rdx, 34
+    lea rcx, [rel s]
+    xor rax, rax
+    call printf wrt ..plt
+    leave
+    ret
 
 section .data
-q db 96
-s db "section .text", 10, "global _start
-
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, s
-    mov rdx, 333
-    syscall
-
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, q
-    mov rdx, 1
-    syscall
-
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, s
-    mov rdx, 333
-    syscall
-
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, q
-    mov rdx, 1
-    syscall
-
-    mov rax, 60
-    xor rdi, rdi
-    syscall
-
-section .data
-q db 96
-s db "
+    s db "section .text%1$c    global main%1$c    extern printf%1$c%1$cmain:%1$c    push rbp%1$c    mov rbp, rsp%1$c    lea rdi, [rel s]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel s]%1$c    xor rax, rax%1$c    call printf wrt ..plt%1$c    leave%1$c    ret%1$c%1$csection .data%1$c    s db %2$c%3$s%2$c, 0", 0
