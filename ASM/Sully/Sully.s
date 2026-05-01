5dadcbb508ecefc50b524b296c51dc4edaf2333e
IzaroArbaiza <147154188+IzaroArbaiza@users.noreply.github.com>
2026-05-01 16:29:54 +0000

    All corrected -> 0-5 + Makefile + Compilation

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -10,25 +10,22 @@ main:
 	push rbp
 	mov rbp, rsp
 
-	;int i = 5;
 	mov edi, 5
-	;int x = i - 1
-	mov esi, edi
-	sub esi, 1
-	mov [x_store], esi
+	mov [x_store], edi
 
-	;if(i < 0)
-	cmp esi, -1
+	cmp dword [x_store], 0
 	jl .exit
 
-	;sprintf(buffer, "Sully_%d.s", x);
+	mov eax, [x_store]
+	sub eax, 1
+	mov [x_next], eax
+
 	mov rdi, buffer
 	mov rsi, form
 	mov edx, [x_store]
 	xor rax, rax
 	call sprintf
 
-	;fopen(buffer, "w");
 	mov rdi, buffer
 	mov rsi, mode
 	call fopen
@@ -36,42 +33,36 @@ main:
 	test rax, rax
 	jz .error
 
-	mov rbx, rax	;guardar FILE*
+	mov rbx, rax
 
-	;fprintf(f, code, ...);
-	mov rdi, rbx	;FILE*
-	mov rsi, code	;code
-	mov rdx, 10		;%1$c = '\n'
-	mov rcx, 9		;%2$c = '\t'
-	mov r8, 34		;%3$c = "
-	mov r9, code	;%4$c = string
+	mov rdi, rbx
+	mov rsi, code
+	mov rdx, 10
+	mov rcx, 9
+	mov r8, 34
+	mov r9, code
 
-	sub rsp, 16
-	mov eax, [x_store]
+	sub rsp, 8
+	mov eax, [x_next]
 	mov [rsp], rax
 
 	xor rax, rax
 	call fprintf
-	add rsp, 16
+	add rsp, 8
 
-	;fclose
 	mov rdi, rbx
 	call fclose
 
-	;COMPILAR
-	;sprintf(compile_cmd, compile, buffer, x)
 	mov rdi, compile_cmd
 	mov rsi, compile
-	mov rdx, buffer		; %1$s
-	mov ecx, [x_store]	; %2$d
+	mov rdx, buffer
+	mov ecx, [x_store]
 	xor rax, rax
 	call sprintf
 
-	;system(compile_cmd)
 	mov rdi, compile_cmd
 	call system
 
-	;EJECUTAR
 	mov rdi, exec_cmd
 	mov rsi, exec
 	mov edx, [x_store]
@@ -96,6 +87,7 @@ section .bss
 	compile_cmd resb 256
 	exec_cmd resb 64
 	x_store resd 1
+	x_next resd 1
 
 section .data
 	form db "Sully_%d.s", 0
@@ -103,4 +95,4 @@ section .data
 	compile db "nasm -f elf64 %1$s -o Sully_%2$d.o && gcc -no-pie Sully_%2$d.o -o Sully_%2$d && rm -f Sully_%2$d.o", 0
 	exec db "./Sully_%d", 0
 
-code db "section .text%1$c%2$cglobal main%1$c%2$cextern sprintf%1$c%2$cextern fopen%1$c%2$cextern fclose%1$c%2$cextern fprintf%1$c%2$cextern system%1$c%1$cmain:%1$c%2$cpush rbp%1$c%2$cmov rbp, rsp%1$c%1$c%2$cmov edi, %5$d%1$c%2$cmov esi, edi%1$c%2$csub esi, 1%1$c%2$cmov [x_store], esi%1$c%2$ccmp esi, -1%1$c%2$cjl .exit%1$c%1$c%2$cmov rdi, buffer%1$c%2$cmov rsi, form%1$c%2$cmov edx, [x_store]%1$c%2$cxor rax, rax%1$c%2$ccall sprintf%1$c%1$c%2$cmov rdi, buffer%1$c%2$cmov rsi, mode%1$c%2$ccall fopen%1$c%2$ctest rax, rax%1$c%2$cjz .error%1$c%2$cmov rbx, rax%1$c%1$c%2$cmov rdi, rbx%1$c%2$cmov rsi, code%1$c%2$cmov rdx, 10%1$c%2$cmov rcx, 9%1$c%2$cmov r8, 34%1$c%2$cmov r9, code%1$c%2$csub rsp, 16%1$c%2$cmov eax, [x_store]%1$c%2$cmov [rsp], rax%1$c%2$cxor rax, rax%1$c%2$ccall fprintf%1$c%2$cadd rsp, 16%1$c%1$c%2$cmov rdi, rbx%1$c%2$ccall fclose%1$c%1$c%2$cmov rdi, compile_cmd%1$c%2$cmov rsi, compile%1$c%2$cmov rdx, buffer%1$c%2$cmov ecx, [x_store]%1$c%2$cxor rax, rax%1$c%2$ccall sprintf%1$c%2$cmov rdi, compile_cmd%1$c%2$ccall system%1$c%1$c%2$cmov rdi, exec_cmd%1$c%2$cmov rsi, exec%1$c%2$cmov edx, [x_store]%1$c%2$cxor rax, rax%1$c%2$ccall sprintf%1$c%2$cmov rdi, exec_cmd%1$c%2$ccall system%1$c%1$c.exit:%1$c%2$cxor eax, eax%1$c%2$cleave%1$c%2$cret%1$c%1$c.error:%1$c%2$cmov eax, 1%1$c%2$cleave%1$c%2$cret%1$c%1$csection .bss%1$c%2$cbuffer resb 64%1$c%2$ccompile_cmd resb 256%1$c%2$cexec_cmd resb 64%1$c%2$cx_store resd 1%1$c%1$csection .data%1$c%2$cform db %3$cSully_%%d.s%3$c, 0%1$c%2$cmode db %3$cw%3$c, 0%1$c%2$ccompile db %3$cnasm -f elf64 %%1$s -o Sully_%%2$d.o && gcc -no-pie Sully_%%2$d.o -o Sully_%%2$d && rm -f Sully_%%2$d.o%3$c, 0%1$c%2$cexec db %3$c./Sully_%%d%3$c, 0%1$ccode db %3$c%4$s%3$c, 0%1$c", 0
+code db "section .text%1$c%2$cglobal main%1$c%2$cextern sprintf%1$c%2$cextern fopen%1$c%2$cextern fclose%1$c%2$cextern fprintf%1$c%2$cextern system%1$c%1$cmain:%1$c%2$cpush rbp%1$c%2$cmov rbp, rsp%1$c%1$c%2$cmov edi, %5$d%1$c%2$cmov [x_store], edi%1$c%1$c%2$ccmp dword [x_store], 0%1$c%2$cjl .exit%1$c%1$c%2$cmov eax, [x_store]%1$c%2$csub eax, 1%1$c%2$cmov [x_next], eax%1$c%1$c%2$cmov rdi, buffer%1$c%2$cmov rsi, form%1$c%2$cmov edx, [x_store]%1$c%2$cxor rax, rax%1$c%2$ccall sprintf%1$c%1$c%2$cmov rdi, buffer%1$c%2$cmov rsi, mode%1$c%2$ccall fopen%1$c%1$c%2$ctest rax, rax%1$c%2$cjz .error%1$c%1$c%2$cmov rbx, rax%1$c%1$c%2$cmov rdi, rbx%1$c%2$cmov rsi, code%1$c%2$cmov rdx, 10%1$c%2$cmov rcx, 9%1$c%2$cmov r8, 34%1$c%2$cmov r9, code%1$c%1$c%2$csub rsp, 8%1$c%2$cmov eax, [x_next]%1$c%2$cmov [rsp], rax%1$c%1$c%2$cxor rax, rax%1$c%2$ccall fprintf%1$c%2$cadd rsp, 8%1$c%1$c%2$cmov rdi, rbx%1$c%2$ccall fclose%1$c%1$c%2$cmov rdi, compile_cmd%1$c%2$cmov rsi, compile%1$c%2$cmov rdx, buffer%1$c%2$cmov ecx, [x_store]%1$c%2$cxor rax, rax%1$c%2$ccall sprintf%1$c%1$c%2$cmov rdi, compile_cmd%1$c%2$ccall system%1$c%1$c%2$cmov rdi, exec_cmd%1$c%2$cmov rsi, exec%1$c%2$cmov edx, [x_store]%1$c%2$cxor rax, rax%1$c%2$ccall sprintf%1$c%1$c%2$cmov rdi, exec_cmd%1$c%2$ccall system%1$c%1$c.exit:%1$c%2$cxor eax, eax%1$c%2$cleave%1$c%2$cret%1$c%1$c.error:%1$c%2$cxor eax, 1%1$c%2$cleave%1$c%2$cret%1$c%1$csection .bss%1$c%2$cbuffer resb 64%1$c%2$ccompile_cmd resb 256%1$c%2$cexec_cmd resb 64%1$c%2$cx_store resd 1%1$c%2$cx_next resd 1%1$c%1$csection .data%1$c%2$cform db %3$cSully_%%d.s%3$c, 0%1$c%2$cmode db %3$cw%3$c, 0%1$c%2$ccompile db %3$cnasm -f elf64 %%1$s -o Sully_%%2$d.o && gcc -no-pie Sully_%%2$d.o -o Sully_%%2$d && rm -f Sully_%%2$d.o%3$c, 0%1$c%2$cexec db %3$c./Sully_%%d%3$c, 0%1$c%1$ccode db %3$c%4$s%3$c, 0%1$c", 0
--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -5,12 +5,12 @@
 int main() {
 	int i = 5;
 	int x = i - 1;
-	char *code = "#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c%cint main() {%c%cint i = %d;%c%cint x = i - 1;%c%cchar *code = %c%s%c;%c%c%cif (i < 0)%c%c%creturn 0;%c%cchar filename[32];%c%csprintf(filename, %cSully_%%d.c%c, x);%c%cFILE *f = fopen(filename, %cw%c);%c%cif (!f)%c%c%creturn 1;%c%cfprintf(f, code, 10,10,10,10,10,9,x,10,9,10,9,34,code,34,10,10,9,10,9,9,10,9,10,9,34,34,10,9,34,34,10,9,10,9,9,10,9,10,9,10,10,9,10,9,34,34,10,9,10,9,10,9,34,34,10,9,10,10,9,10);%c%cfclose(f);%c%c%cchar compile_cmd[128];%c%csprintf(compile_cmd, %cgcc -Wall -Wextra -Werror %%s -o Sully_%%d%c, filename, x);%c%csystem(compile_cmd);%c%cchar exec_cmd[64];%c%csprintf(exec_cmd, %c./Sully_%%d%c, x);%c%csystem(exec_cmd);%c%c%creturn 0;%c}";
+	char *code = "#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c%cint main() {%c%cint i = %d;%c%cint x = i - 1;%c%cchar *code = %c%s%c;%c%c%cif (i < 0)%c%c%creturn 0;%c%cchar filename[32];%c%csprintf(filename, %cSully_%%d.c%c, i);%c%cFILE *f = fopen(filename, %cw%c);%c%cif (!f)%c%c%creturn 1;%c%cfprintf(f, code, 10,10,10,10,10,9,x,10,9,10,9,34,code,34,10,10,9,10,9,9,10,9,10,9,34,34,10,9,34,34,10,9,10,9,9,10,9,10,9,10,10,9,10,9,34,34,10,9,10,9,10,9,34,34,10,9,10,10,9,10);%c%cfclose(f);%c%c%cchar compile_cmd[128];%c%csprintf(compile_cmd, %cgcc -Wall -Wextra -Werror %%s -o Sully_%%d%c, filename, i);%c%csystem(compile_cmd);%c%cchar exec_cmd[64];%c%csprintf(exec_cmd, %c./Sully_%%d%c, i);%c%csystem(exec_cmd);%c%c%creturn 0;%c}";
 
 	if (i < 0)
 		return 0;
 	char filename[32];
-	sprintf(filename, "Sully_%d.c", x);
+	sprintf(filename, "Sully_%d.c", i);
 	FILE *f = fopen(filename, "w");
 	if (!f)
 		return 1;
@@ -18,10 +18,10 @@ int main() {
 	fclose(f);
 
 	char compile_cmd[128];
-	sprintf(compile_cmd, "gcc -Wall -Wextra -Werror %s -o Sully_%d", filename, x);
+	sprintf(compile_cmd, "gcc -Wall -Wextra -Werror %s -o Sully_%d", filename, i);
 	system(compile_cmd);
 	char exec_cmd[64];
-	sprintf(exec_cmd, "./Sully_%d", x);
+	sprintf(exec_cmd, "./Sully_%d", i);
 	system(exec_cmd);
 
 	return 0;
